74db9e2888d918794e6fdd030b5345860805fc51
Jonna Mohit <luckymohit789@gmail.com>
2026-02-23 09:22:59 +0000

    Remove docs folder and ignore it

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('clickMe');
-    button.addEventListener('click', () => {
-        alert('Button clicked!');
-    });
-});
